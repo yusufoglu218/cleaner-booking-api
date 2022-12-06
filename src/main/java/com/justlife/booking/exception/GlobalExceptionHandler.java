bf09953ec30f49ba38ad4afd3854b5111d3799c5
omer.yusufoglu@vodafone.com <omer.yusufoglu@vodafone.com>
2022-12-06 10:39:27 +0300

    add log

--- src/main/java/com/justlife/booking/exception/GlobalExceptionHandler.java
+++ src/main/java/com/justlife/booking/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.justlife.booking.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,18 +12,21 @@ import java.util.Date;
 /**
  * Exception handler to handle custom exception and set error details
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RecordNotFoundException.class)
     public ResponseEntity<?> recordNotFoundException(RecordNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        log.error("Data not found error: {}", ex.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        log.error("Error occurred: {}", ex.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
