1ae6ff0ebfe4d881caad720b070d2d9bf9cbfc6b
omer.yusufoglu@vodafone.com <omer.yusufoglu@vodafone.com>
2022-12-06 10:38:13 +0300

    add error message for timePeriod

--- src/main/java/com/justlife/booking/exception/ErrorType.java
+++ src/main/java/com/justlife/booking/exception/ErrorType.java
@@ -4,7 +4,8 @@ package com.justlife.booking.exception;
  * Enums for error description and internal error code
  */
 public enum ErrorType {
-    BOOKING_NOT_FOUND("Booking not found with id: ");
+    BOOKING_NOT_FOUND("Booking not found with id: "),
+    TIME_PERIOD_NOT_FOUND("TimePeriod not found with id: ");
 
     private final String description;
 
